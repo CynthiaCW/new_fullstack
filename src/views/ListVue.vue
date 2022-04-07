f255336a137d8dac868c3317131a7ae4945d65d2
GabrieleCapocelli <93205404+GabrieleCapocelli@users.noreply.github.com>
2022-04-07 12:15:46 +0200

    primi problemi

--- src/views/ListVue.vue
+++ src/views/ListVue.vue
@@ -1,22 +1,17 @@
 <template>
     <div>
-        <div class="courses-container">
+        <div class="courses-container" v-for="record in vinyls" :key="record.id">
             <h1>Découvrez nos vinyls</h1>
             <div class="course">
                 <div class="course-preview" :style="{ backgroundColor: cardColor }">
                     <slot>
                         <h2>En stock</h2>
-                        <img
-                            class="main-image"
-                            src="static/img/81aTawcGdmL._SL1500_.jpg"
-                            height="200px"
-                            href="#"
-                        />
+                        <img class="main-image" :src="record.picture_link" height="200px" href="#" />
                     </slot>
                 </div>
                 <div class="course-info">
-                    <h6>Nom du groupe {{ chapNumber }}</h6>
-                    <h2>Titre du vinyls{{ Title }}</h2>
+                    <h6>{{ record.band }}</h6>
+                    <h2>Titre du vinyls{{ record.title }}</h2>
 
                     <button class="btn" @click="selectCourse" href="#VinylsDetails.html">Voir</button>
                 </div>
@@ -29,14 +24,23 @@
 
 <script>
 import axios from 'axios'
-const API = 'localhost:8000/read.php'
+const API = 'http://localhost:8000/read.php'
 
-export default{
+export default {
     name: 'ListVue',
-    data:()=>({
-        vinyls:[]
-    })
+    data: () => ({
+        vinyls: []
+    }),
+    async created() {
+        const vinyls = await axios.get(API)
+        this.vinyls = vinyls.data
+    }
 }
 
 
 </script>
+
+
+<style scoped>
+@import "../assets/static/css/vinyl_list.css";
+</style>

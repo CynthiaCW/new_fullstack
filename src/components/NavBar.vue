e34d0d28c88e912dd571476810f7e1aea3dbd94b
CynthiaCW <cynthia.colombelwnek@gmail.com>
2022-04-07 11:53:23 +0200

    css

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <nav class="navbar">
+   
+    <ul>
+      <img class="navbar-brand" src="static/img/logo.png" height='100px'>
+
+
+      <li class="nav-item active">
+        <a class="nav-link" href="">Se connecter</a>
+      </li>
+      
+      <li class="nav-item active">
+        <a class="nav-link" href="">Nos Vinyls</a>
+      </li>
+
+      
+
+    </ul>
+  </nav>
+
+  <p>Decouvrer nos vinyls</p>
+    </div>
+</template>

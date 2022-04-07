74312ec418a3542b8c6ea7a5d9a48295191f2659
CynthiaCW <cynthia.colombelwnek@gmail.com>
2022-04-07 13:40:09 +0200

    dETAILS

---
+++ src/components/NavBar.vue
@@ -0,0 +1,70 @@
+<template>
+      <nav class="navbar">
+   
+    <ul>
+      <img class="navbar-brand" src="" height='100px'> 
+        <!-- todo src -->
+
+        <li class="nav-item active">
+        <a class="nav-link" href="">Home</a>
+      </li>
+
+      <li class="nav-item active">
+        <a class="nav-link" href="">Se connecter</a>
+      </li>
+      
+      <li class="nav-item active">
+        <a class="nav-link" href="">Nos Vinyls</a>
+      </li>
+
+      
+
+    </ul>
+  </nav>
+
+</template>
+
+<style scoped>
+
+.navbar {
+    background-color: rgb(34, 34, 34);
+    margin: 15px;
+    height: 100%;
+    width: 400px;
+    position: fixed; /* Fixed Sidebar (stay in place on scroll) */
+    z-index: 1; /* Stay on top */
+    top: 0; /* Stay at the top */
+    left: 0;
+    overflow-x: hidden; /* Disable horizontal scroll */
+    padding-top: 20px;
+    border-radius: 25px;
+    border: #7B6DED;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+
+ul {
+    padding-left: 40px;
+}
+
+ul>li {
+    text-decoration: none;
+    list-style-type: none;
+    padding: 40px;
+    display: block;
+
+}
+ul>li>a {
+    text-decoration: none;
+    font-family: 'Poppins';
+    font-size: 1.5rem;
+    color: white;
+
+}
+
+ul>li>a:hover {
+    color: #7B6DED;
+}
+
+/*---------------------------*/
+
+</style>
--- src/views/ListVue.vue
+++ src/views/ListVue.vue
@@ -1,19 +1,23 @@
-<div>
+<template>
+    <div>
         <div class="courses-container">
           <h1>Découvrez nos vinyls</h1>
             <div class="course">
-                <div class="course-preview" :style="{backgroundColor:cardColor}">
+                <div class="course-preview">
                     <slot>
                         <h2>En stock</h2>
                         <img class="main-image" src="static/img/81aTawcGdmL._SL1500_.jpg" height='200px' href="#"/>
                     </slot>
                 </div>
                 <div class="course-info">
-                    <h6>Nom du groupe {{chapNumber}}</h6>
-                    <h2>Titre du vinyls{{Title}}</h2>
+                    <h6>Nom du groupe </h6>
+                    <h2>Titre du vinyls</h2>
       
-                    <button class="btn" @click="selectCourse" href="#VinylsDetails.html">Voir</button>
+                    <button class="btn"  href="">Voir</button> 
+                    <!-- @click="selectCourse" -->
                 </div>
             </div>
         </div>
       </div>
+</template>
+
